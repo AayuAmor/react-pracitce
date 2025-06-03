abb845a00e00906a19ee72251fb89c9a39beb506
AayuAmor <aayuamor@gmail.com>
2025-06-03 18:25:46 +0545

    Editing practice.jsx file to solve interview question

--- src/Practice.jsx
+++ src/Practice.jsx
@@ -1,15 +1,18 @@
 export const Practice = () => {
     // return <h1>Interview</h1>
-    const student=[];
+    const student=[1,2,3,4];
+    console.log(Boolean(student.length))
+    console.log(student.length)
     return(
-        <>
-       <p> {student.length===0 && "No one is Presesnt"}</p>
+    <>
+       {/* 1st method */}
+       {/* <p> {student.length===0 && "No one is Presesnt"}</p> */}
+       {/* 2nd Method */}
+       {/* <p>{!student.length && "No students Found"}</p> */}
+       {/* 3rd Method */}
+       <p>{Boolean(!student.length) && "No Students Found"}</p>
        <p>Number of Students: {student.length}</p>
         
-        </>
-        
-
-    );
-    
-    
+    </>
+    ); 
 }
